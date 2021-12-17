92b5ae0f2d776d807f5edce454259c1ccb0a412a
OscarJVD <oscarjaviervargas@hotmail.com>
2021-12-17 16:42:35 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-const idpURL = `https://my.local.host:50000/oidc`;
-// const idpURL = `https://oidc.soyyoadmin.com/oidc`;
+// const idpURL = `https://my.local.host:50000/oidc`;
+const idpURL = `https://oidc.soyyoadmin.com/oidc`;
 const spURL = `https://my.local.host:4200`;
 
-const clientID = `foo`;
-const secretID = `foo`;
-// const clientID = `s6U1ZxrkqUWI0b3AI27ep2T3xUX2iKaMKQFWutNTPB4rPbx`;
-// const secretID = `50L8evxHkUL0WDsX2L2fBWPjAxJvzmCh26FIwNVLOloN9zNfIcyUNuK4HwKLEhWHpCo3fq`;
+// const clientID = `foo`;
+// const secretID = `foo`;
+const clientID = `s6U1ZxrkqUWI0b3AI27ep2T3xUX2iKaMKQFWutNTPB4rPbx`;
+const secretID = `50L8evxHkUL0WDsX2L2fBWPjAxJvzmCh26FIwNVLOloN9zNfIcyUNuK4HwKLEhWHpCo3fq`;
 
 const redirectURI = `${spURL}/registro`;
 const respType = `code`;
@@ -44,44 +40,4 @@ export const environment = {
   idpTokenURL: idpTokenURI,
 
   logout: `${logoutURL}?client_id=${clientID}&response_type=${respType}&scope=${respScope}&redirect_uri=${redirectURI}&code_challenge=${codeChallenge}&code_challenge_method=${codeChallenge_method}&code_verifier=${codeVerifier}&post_logout_redirect_uri=${spURL}`,
-
-  // &post_logout_redirect_uri=${spURL}&id_token_hint=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJhdWQiOiJodHRwczovL2F1dGgucGluZ29uZS5jb20vL2FzIiwic3ViIjoiIiwiaWF0IjoxNjM4OTA0NjQ0LCJleHAiOjE2Mzg5OTEzNDR9.U1XESS29Bohleeqgzeqfl4W3adJdmoz2i-UUEXlFfyw
-  // &post_logout_redirect_uri=${spURL}
-  // error_description: could not decode id_token_hint
 };
-/*
-client_id: '3chjqdttb8pfum7pchbfue9gop',
-  secret_id: '1t1mhti23acf0j1lknelh0amq895v8pg4o2o75jd6mmm81c5vbd8',
-
-  loginURL: 'https://registroidp-soyyo.auth.us-east-2.amazoncognito.com/login?' +
-              'client_id=3chjqdttb8pfum7pchbfue9gop&response_type=code&scope=email&issuer:https://cognito-idp.us-east-2.amazonaws.com/us-east-2_xh6BdcnD3&redirect_uri=http://localhost:4200/registro',
-
-  redirectURL: 'http://localhost:4200/registro',
-
-  idpTokenURL: 'https://registroidp-soyyo.auth.us-east-2.amazoncognito.com/oauth2/token',
-  logout_uri:'http://localhost:4200/login',
-  logout: 'https://registroidp-soyyo.auth.us-east-2.amazoncognito.com/logout?' +
-  'client_id=3chjqdttb8pfum7pchbfue9gop&response_type=code&scope=email&issuer:https://cognito-idp.us-east-2.amazonaws.com/us-east-2_xh6BdcnD3&logout_uri=http://localhost:4200/login',
-*/
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
-/* client_id: '3j0124fpl7r7m2ibppnkmahtk2',
-  secret_id: '1qo1sb7b99850bshc50lvk48rncitgnoe6cbodqdlb40llg46skf',
-
-  loginURL: 'https://registro-idp-soyyo.auth.us-east-1.amazoncognito.com/login?' +
-              'client_id=3j0124fpl7r7m2ibppnkmahtk2&response_type=code&scope=email&issuer:https://cognito-idp.us-east-2.amazonaws.com/us-east-1_BHd5PMa09&redirect_uri=http://localhost:4200/registro',
-
-  redirectURL: 'http://localhost:4200/registro',
-
-  idpTokenURL: 'https://registro-idp-soyyo.auth.us-east-1.amazoncognito.com/oauth2/token',
-  logout_uri:'http://localhost:4200/login',
-  logout: 'https://registro-idp-soyyo.auth.us-east-1.amazoncognito.com/logout?' +
-          'client_id=3j0124fpl7r7m2ibppnkmahtk2&response_type=code&scope=email&issuer:https://cognito-idp.us-east-2.amazonaws.com/us-east-1_BHd5PMa09&logout_uri=http://localhost:4200/login',
-¨*/
